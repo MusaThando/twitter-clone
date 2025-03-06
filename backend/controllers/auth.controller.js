5f9f96a8854ca876f2ddd1a2cf145a53b63c978c
Nkosinathi Mngezane <nmngezane@gmail.com>
2025-03-06 20:00:30 +0200

    1 - Project Setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const SignUp = async (req,res) =>{
+    res.json({"data":"You have hit the sign up endpoit"})
+}
+
+export const Login = async (req,res) =>{
+    res.json({"data":"You have hit the sign up endpoit"})
+}
+
+export const Logout = async (req,res) =>{
+    res.json({"data":"You have hit the sign up endpoit"})
+}
+
