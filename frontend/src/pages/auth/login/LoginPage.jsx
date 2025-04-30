3e04334f59fae5e79d9ce2e8634df5c3f021f434
Nkosinathi Mngezane <nmngezane@gmail.com>
2025-04-30 21:15:09 +0200

    3-Auth handled and routes protected in client

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -5,6 +5,8 @@ import XSvg from "../../../components/svgs/X";
 
 import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
+import {useMutation,useQueryClient} from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 
 const LoginPage = () => {
@@ -13,10 +15,39 @@ const LoginPage = () => {
 		password: "",
 	});
 
+	const queryClient = useQueryClient();
+
+	const {mutate:LoginMutation,isError,isPending,error} = useMutation({
+		mutationFn: async ({username,password}) => {
+			try {
+				const res = await fetch("api/auth/login",{
+					method:"POST",
+					headers:{
+						"Content-Type":"application/json",
+					},
+					body:JSON.stringify({username,password}),
+				});
+
+				const data = await res.json();
+
+				if(!res.ok) throw new Error(data.message || "Login failed");
+
+				return data;
+
+
+			} catch (error) {
+				throw new error;
+			}
+		},
+		onSuccess:() =>{
+			// refetch the authUser
+			queryClient.invalidateQueries({ queryKey: ["authUser"] })
+		}
+	});
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(formData);
+		LoginMutation(formData);
 	};
 
 	const handleInputChange = (e) => {
@@ -55,14 +86,12 @@ const LoginPage = () => {
 							value={formData.password}
 						/>
 					</label>
-					<button className='btn rounded-full btn-primary text-white'>
-            Login
-						{/* {isPending ? "Loading..." : "Login"} */}
-					</button>
-					{/* {isError && <p className='text-red-500'>{error.message}</p>} */}
+					<button className='btn rounded-full btn-primary text-white'>{isPending? "Loading...": "Login"}</button>
+					{isError && <p className='text-red-500'>{error.message}</p>}
+					
 				</form>
 				<div className='flex flex-col gap-2 mt-4'>
-					<p className='text-white text-lg'>{"Don't"} have an account?</p>
+					<p className='text-white text-lg'>Don't have an account?</p>
 					<Link to='/signup'>
 						<button className='btn rounded-full btn-primary text-white btn-outline w-full'>Sign up</button>
 					</Link>
