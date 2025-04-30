9d74c4f8a62b47fb3ecff0396661eac291340427
Nkosinathi Mngezane <nmngezane@gmail.com>
2025-04-30 22:23:31 +0200

    3- fetch posts for the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
-import { useState ,React} from 'react';
-import {Navigate, Route, Routes} from 'react-router-dom';
-import HomePage from './pages/home/HomePage';
-import LoginPage from './pages/auth/login/LoginPage';
-import SignUpPage from './pages/auth/signup/SignUpPage'
-import Sidebar from './components/common/Sidebar';
-import RightPanel from './components/common/RightPanel';
-import NotificationPage from './pages/notification/NotificationPage';
-import ProfilePage from './pages/profile/ProfilePage';
-import {Toaster} from "react-hot-toast";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+
+import { Toaster } from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "./components/common/LoadingSpinner";
 
 function App() {
-
-  const { data: authUser, isLoading } = useQuery({
+	const { data: authUser, isLoading } = useQuery({
 		// we use queryKey to give a unique name to our query and refer to it later
 		queryKey: ["authUser"],
 		queryFn: async () => {
@@ -40,23 +41,22 @@ function App() {
 			</div>
 		);
 	}
-  return (
 
-    <div className='flex max-w-6xlm mx-auto'>
-   
-      {authUser && <Sidebar/>}
-      <Routes>
-        <Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			{/* Common component, bc it's not wrapped with Routes */}
+			{authUser && <Sidebar />}
+			<Routes>
+				<Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
 				<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
 				<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to='/' />} />
 				<Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to='/login' />} />
 				<Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
-      </Routes>
-      {authUser && <RightPanel/>}
-      <Toaster/>
-    </div>
-
-  )
+			</Routes>
+			{authUser && <RightPanel />}
+			<Toaster />
+		</div>
+	);
 }
 
-export default App
+export default App;
